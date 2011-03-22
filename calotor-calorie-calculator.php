5e04983b5fd5e758b15b2f417f7b22262280dd67
thedark <thedark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-22 17:50:48 +0000

    git-svn-id: https://plugins.svn.wordpress.org/calotor-calorie-calculator/tags/0.2@363358 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ calotor-calorie-calculator.php
@@ -0,0 +1,67 @@
+<?php
+/*
+Plugin Name: Calotor Calorie Counter
+Plugin URI: http://www.calotor.com
+Description: Figure out your basal metabolic rate
+Author: Lucian Apostol
+Version: 0.2
+Author URI: http://www.lucianapostol.com
+*/
+
+function calotor() 
+{
+  ?>
+	<script type="text/javascript">
+		function mod(div,base) {
+				return Math.round(div - (Math.floor(div/base)*base));
+		}
+
+
+		function bmr_calculator(formhandler)  {
+			
+			var w = formhandler.bmr_weight.value * 1;
+			var a = formhandler.bmr_age.value * 1;
+
+			displaybmr = (Math.round( (10 * w * 0.4535 ) + ( 5 * 180 ) - ( 5 * a )  + 66.5       ));
+			var rvalue = true;
+			if ( (w <= 35) || (w >= 500)  || (a <= 1) || (a >= 120) ) {
+				document.getElementById("bmr_result").innerHTML = "Invalid data";
+				rvalue = false;
+				return false;
+			}
+			if (rvalue) {
+					document.getElementById("bmr_result").innerHTML = "Your BMR is: " + displaybmr;
+			}
+
+
+			return false;
+		}
+	</script> 
+
+	<form id="calotorform" onsubmit="return bmr_calculator(this);" method="post">
+		Weight: <input type="text" name="bmr_weight" id="bmi_weight" size="9"; /> lbs.<br>
+		Age: <input type="text" name="bmr_age" id="bmr_age" size="9"; /> years <br>
+		<br><input type="submit" name="submit" id="submit" value="Calculate" /><br>
+		<div id="bmr_result"></div>
+	</form>
+
+  <?
+}
+
+function widgetCalotor($args) {
+  extract($args);
+  echo $before_widget;
+  echo $before_title;?>BMI calculator<?php echo $after_title;
+  calotor();
+  echo $after_widget;
+}
+
+function calotorInit()
+{
+  register_sidebar_widget(__('Calotor'), 'widgetCalotor');     
+  add_shortcode( 'calotor', 'calotor' );
+}
+add_action("plugins_loaded", "calotorInit");
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Plugin Name ===
+Contributors: Lucian Apostol
+Donate link: http://www.calotor.com
+Tags: health, calories, weight loss, tools, widgets, calculator
+Requires at least: 2.5.2
+Tested up to: 3.0.4
+Stable tag: 0.2
+
+Provide your visitors the ability to figure out their basal metabolic rate, based on their age, sex and weight.
+
+== Description ==
+
+You can add the plugin as a widget, dragging and droppint it from the wordpress administration panel, Appearance->Widgets section, or with a short tag directly in your blog posts or pages using the [calotor] shortcode.
+
+You can also add it directly into your template files.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload the plugin to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. To add the plugin to your pages, either go to Appearance->Widgets and drag the Calotor calorie counter widget to one of your sidebars, or add the following shortcode to your wordpress pages and posts.
+
+== Frequently Asked Questions ==
+
+= Do i have to change my theme to make the plugin work? =
+
+No theme changes are needed for this plugin. 
+
+= How it will affect my design and blog functionality =
+
+The blog functionality will not be affected in any way.
+
+== Changelog ==
+
+= 0.1 =
+* This is the first version of the plugin. Any suggestions and feedback is highly appreciated.
