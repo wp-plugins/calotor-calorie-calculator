f4d217ed7ecc97bd89e34041ef1d492cd02965c9
thedark <thedark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-23 10:53:57 +0000

    plugin adapted to wordpress 4.0.1
    git-svn-id: https://plugins.svn.wordpress.org/calotor-calorie-calculator/trunk@1031119 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- calotor-calorie-calculator.php
+++ calotor-calorie-calculator.php
@@ -4,7 +4,7 @@ Plugin Name: Calotor Calorie Counter
 Plugin URI: http://www.calotor.com
 Description: Figure out your basal metabolic rate
 Author: Lucian Apostol
-Version: 1.1
+Version: 1.2
 Author URI: http://www.lucianapostol.com
 */
 
